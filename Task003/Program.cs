63e2f1af96e42b5ed5afb3be1ddb5efba274e40d
Stanislav <mamontovsv@gmail.com>
2022-05-30 19:08:41 +0500

    Task 3

---
+++ Task003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number%2 == 0)
+{
+    Console.Write("Это четное число!");
+}
+else
+{
+    Console.Write("Это нечетное число!");
+}
