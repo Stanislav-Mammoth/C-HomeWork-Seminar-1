20d1185016319e644b1eaf7535e0f1b9f1359e85
Stanislav <mamontovsv@gmail.com>
2022-05-30 20:26:06 +0500

    Task 6

---
+++ Task006/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите любое число в диапазоне от 10 до 99 включительно: ");
+int number = int.Parse(Console.ReadLine());
+if (number<10 | number>99)
+{
+    Console.Write("Введите число в диапазоне!");
+}
+else {
+        if (number/10 > number%10) Console.Write("Первая цифра больше!");
+  
+        if (number/10 < number%10) Console.Write("Вторая цифра больше!");
+  
+        if (number/10 == number%10) Console.Write("Цифры равны!");
+}
+
+
