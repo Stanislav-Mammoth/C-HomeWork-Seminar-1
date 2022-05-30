f1d7bff04bf1920cfd20954d81e75fdab421aa57
Stanislav <mamontovsv@gmail.com>
2022-05-30 20:04:05 +0500

    Task 5

---
+++ Task005/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите любое трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+if (number<100 | number>999)
+{
+    Console.Write("Вы ввели не трехзначное число!");
+}
+else
+{
+    Console.Write("Предпоследняя цифра введенного числа: ");
+    Console.Write((number/10)%10);
+}
