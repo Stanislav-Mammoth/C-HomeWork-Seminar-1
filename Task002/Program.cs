d8875ee51f685e5b2307ae3426a0b3bcffb0f01a
Stanislav <mamontovsv@gmail.com>
2022-05-30 18:48:26 +0500

    Task 2

---
+++ Task002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number_3 = int.Parse(Console.ReadLine());
+
+int max = number_1;
+
+if (number_2 > number_1) max = number_2;
+if (number_3 > number_2) max = number_3;
+
+Console.Write("Максимальное число: ");
+Console.Write(max);
