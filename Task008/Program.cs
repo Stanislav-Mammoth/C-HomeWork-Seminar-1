9a9d20f8ed44468169bb49501ef5abd6b2b6d2f4
Stanislav <mamontovsv@gmail.com>
2022-06-01 15:12:19 +0500

    Task 8

---
+++ Task008/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Программа для проверки кратности чисел. Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+Console.Write("Кратность какому числу нужно проверить: ");
+int n = int.Parse(Console.ReadLine());
+
+int remainder = (number%n);
+
+if (remainder == 0)
+{
+    Console.Write("Это число кратно ");
+    Console.Write((n));
+}
+else
+{
+    Console.Write("Это число не кратно ");
+    Console.WriteLine((n));
+    Console.Write("Остаток от деления: ");
+    Console.Write((remainder));
+
+}
